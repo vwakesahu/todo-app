64365e96e1c64110aaf238c7dd8e749d88429e92
vwakesahu <viveksahu1762@gmail.com>
2024-01-08 00:36:31 +0530

    Added Search Field

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,7 +4,16 @@ const Menu = () => {
   return (
     <div className="h-screen w-[55%] ">
       <div className="h-[93%] bg-gray-100 border rounded-xl mr-4 p-4 ">
-        <div className="font-semibold text-lg text-headingColor">Menu</div>
+        <p className="font-semibold text-lg text-headingColor">Menu</p>
+
+        {/* Search Field */}
+        <div className="mt-6">
+          <input
+            type="text"
+            className="w-full bg-transparent border px-3 py-2 rounded-lg focus-within:outline-none focus:border-gray-300"
+            placeholder="Search"
+          />
+        </div>
       </div>
     </div>
   );
