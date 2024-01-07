ff65a77f2e9afeb7d49c93ffa5ecaab962fc620d
vwakesahu <viveksahu1762@gmail.com>
2024-01-08 00:39:53 +0530

    this index.js keeps all components and helps to make my imports look cleaner

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Menu } from "./Menu.jsx";
+export { default as Todo } from "./Todo.jsx";
+export { default as AddTodo } from "./AddTodo.jsx";
+export { default as TodoView } from "./TodoView.jsx";
+export { default as TaskList } from "./TaskList.jsx";
+
