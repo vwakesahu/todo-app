5b0a1e4cacb0ddeddd05ec5805aa9531822d56b0
vwakesahu <viveksahu1762@gmail.com>
2024-01-08 00:39:05 +0530

    Initialized TodoView to view each todo separately

---
+++ src/components/TodoView.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TodoView = () => {
+  return (
+    <div className="h-screen w-[55%] ">
+      <div className="h-[93%] bg-gray-100 border rounded-xl mr-4 p-4 ">
+        <div className="font-semibold text-lg text-headingColor">Task:</div>
+      </div>
+    </div>
+  );
+};
+
+export default TodoView;
