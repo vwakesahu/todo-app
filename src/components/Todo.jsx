48eb65aa61192f5c2edc20190e6e43bb4c4eb31e
vwakesahu <viveksahu1762@gmail.com>
2024-01-08 00:37:36 +0530

    Some Ui changes and rendered the TaskList component in TODO.jsx

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { IoAddOutline } from "react-icons/io5";
-const Todo = () => {
+import { TaskList } from ".";
+const Todo = ({ tasks }) => {
+  const [isNewTask, setIsNewTask] = useState(false);
+
+  const handleNewTask = () => {
+    setIsNewTask(!isNewTask);
+  };
   return (
     <div className="h-screen w-full ">
       <div className="h-[93%] mr-4 p-4 ">
@@ -8,18 +15,30 @@ const Todo = () => {
           <p>Today</p>
           <div className="text-sm font-medium">
             <p>
-              Remaining Tasks: <span className="text-red-600 font-bold">6</span>
+              Remaining Tasks: <span className="text-red-600 font-bold">1</span>
             </p>
           </div>
         </div>
 
         {/* Add new task */}
-        <div className="w-full border flex items-center gap-4 mt-10 px-2 py-3 rounded-lg cursor-pointer hover:bg-gray-200">
+        <div
+          className="w-full border flex items-center gap-4 mt-10 px-2 py-3 rounded-lg cursor-pointer hover:bg-gray-200"
+          onClick={handleNewTask}
+        >
           <p>
             <IoAddOutline />
           </p>
           <p className="text-sm">Add New Task</p>
         </div>
+
+        {isNewTask ? (
+          <div className="mt-2 px-2 py-3"> Add New Task</div>
+        ) : (
+          <div className="mt-2">
+            {/* Listing out tasks */}
+            <TaskList />
+          </div>
+        )}
       </div>
     </div>
   );
