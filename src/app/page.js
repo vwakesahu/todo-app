33d95c06c575e8fad942e00c5cd2034b300d7a70
vwakesahu <viveksahu1762@gmail.com>
2024-01-08 19:42:08 +0530

    Added StateProvider

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,16 @@
-import { Menu, Todo, TodoView } from "@/components";
+"use client";
 import React from "react";
+import App from "./_app";
+import { StateProvider } from "@/context/StateProvider";
+import { initialState } from "@/context/intialState";
+import reducer from "@/context/reducer";
 
 const page = () => {
-  const tasks = [];
   return (
-    <div className="w-full flex justify-between p-5 fixed">
-      <Menu />
-      <Todo tasks={tasks} />
-      <TodoView />
+    <div>
+      <StateProvider initialState={initialState} reducer={reducer}>
+        <App />
+      </StateProvider>
     </div>
   );
 };
